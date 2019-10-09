d6d0d3438682e01c4de267b5d05c2f29f84c13cf
Chen-yu Hsieh <lawtrooper@gmail.com>
2019-10-09 09:17:50 +0800

    Change container breakpoints width

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1280px) { ... }
+    }
+  }
+}
