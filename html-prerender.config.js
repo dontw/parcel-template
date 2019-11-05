f21166f5dfa84dd08178e1c0bc58ab336923d5c1
chenyu-hsieh <chenyu-hsieh@tp.carnival.com.tw>
2019-11-05 10:16:03 +0800

    Move summerSale to html folder

---
+++ html-prerender.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: process.env.CURR_ENV,
+  title: 'Good Buy Summer 全館85折最後倒數！台灣設計師原創服飾平台-Halfme',
+  description:
+    '結合設計、原創、共享的服飾平台，為台灣在地新銳設計師提供盡情揮灑的創作舞台，將時尚伸展台延伸到每個人的衣櫃裡，在日常生活中實踐繽紛多元的穿搭美學。',
+  keywords:
+    'vogue、時尚、設計、原創、品牌、設計師、時裝、服飾、男裝、女裝、購物、流行、品牌、穿搭、形象、日常、逛街、shopping、ootd、獨家、優惠、折扣、消費、風格、style、態度、藝術、跨界、品味、名人、文化、潮流、話題、焦點、城市、city、輕奢、現代、當代、趨勢、美學、春夏、秋冬、訂製、系列、款式、旗艦店、旗艦館、概念',
+  url: process.env.URL,
+  faviconUrl: process.env.URL + '/' + 'favicon.ico',
+  imageUrl: process.env.URL + '/' + 'header_share_banner.png'
+};
